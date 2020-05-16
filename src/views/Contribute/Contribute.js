4a8c5e603b8c34a8aaa946a0cf2ebbb7e0de8b14
Jeesu-Jung <jisu.jung5@gmail.com>
2020-05-16 18:30:45 +0900

    translate to contribute page

--- src/views/Contribute/Contribute.js
+++ src/views/Contribute/Contribute.js
@@ -1,8 +1,55 @@
 import React, {Component} from 'react';
-
+import {
+  Button,
+  Col,
+  Input,
+  Row,
+  Card,
+  CardBody,
+} from 'reactstrap';
 class Contribute extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      KoreanText:this.props.location.state.KoreanText,
+      Translated_Text: this.props.location.state.Translated_Text,
+      able_submit: false
+    };
+  }
+  handleChange=(e)=>{
+    this.setState({
+      KoreanText: e.target.value
+    })
+  }
+  handleClick=(e)=>{
+    console.log(this.state.KoreanText);
+    this.setState({
+      Translated_Text:this.state.KoreanText,
+      able_submit:true
+    });
+  }
   render() {
-    return <div>Contribute</div>;
+    return (
+    <div>
+      <Row>
+        <Col>
+        <Card>
+          <CardBody rows="9">
+            {this.state.KoreanText} 
+          </CardBody>
+        </Card>
+          <Button disabled={true} onClick={this.handleClick}>Translate</Button>
+        </Col>
+        <Col>
+          <Card>
+            <Input type="textarea" value={this.state.Translated_Text}
+            onChange={this.handleChange} rows="9"/>
+          </Card>
+          <Button disabled={this.state.able_submit} onClick={this.handleSubmit}>Submit</Button>
+        </Col>          
+      </Row>
+    </div>
+    );
   }
 }
 
--- src/views/Translate/Translate.js
+++ src/views/Translate/Translate.js
@@ -1,4 +1,6 @@
-import React, {Component, SetState} from 'react';
+import React, {Component} from 'react';
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+import Contribute from '../Contribute/Contribute';
 import {
   Button,
   Col,
@@ -12,8 +14,10 @@ class Translate extends Component {
     super(props);
     this.state = { 
       KoreanText:"",
-      Translated_Text: "" 
+      Translated_Text: "" ,
+      able_submit: false
     };
+    
     this.handleClick = this.handleClick.bind(this);
   };
   handleChange=(e)=>{
@@ -22,7 +26,13 @@ class Translate extends Component {
     })
   }
   handleClick=(e)=>{
-    console.log(this.state.KoreanText);
+    let dummy_text = "Deep learning has emerged as a new area of machine-learning research and has been successfully applied to natural language processing, such as machine translation and sentence classification. In this work, we use effective Korean input token units to encode Korean sentences for classification problems, such as topic detection. Recurrent and convolutional neural networks for Korean sentence encoding are briefly reviewed, and various Korean input tokens units, including character, morpheme-tag, morpheme, word, subword, syllable window, and hybrids of morpheme and character methods are explored. Extensive experiments on sentimental analysis, topic detection, and intention understanding tasks are conducted to find effective input token units.";
+    this.setState({
+      Translated_Text:dummy_text,
+      able_submit:true
+    });
+  }
+  handleSubmit=(e)=>{                                                                                     
     this.setState({
       Translated_Text:this.state.KoreanText,
       Korean : ""
@@ -35,19 +45,23 @@ class Translate extends Component {
         <Col>
           <Input type="textarea" value={this.state.KoreanText} 
           onChange={this.handleChange} rows="9"/>
+          <Button onClick={this.handleClick}>Translate</Button>
         </Col>
         <Col>
-        
           <Card>
             <CardBody>
               {this.state.Translated_Text}
             </CardBody>
           </Card>
-        </Col>
-                      
+          <Link to={{pathname: '/contribute',
+            state: {
+              KoreanText: this.state.KoreanText,
+              Translated_Text: this.state.Translated_Text
+          }}}>
+            <Button disabled={!this.state.able_submit} onClick={this.handleSubmit}>Contribute</Button>
+          </Link>
+        </Col>          
       </Row>
-      <Button onClick={this.handleClick}>Translate</Button>
-      {/* <Button onClick={this.handleSubmit}>Contribute</Button> */}
     </div>
     );
     }
