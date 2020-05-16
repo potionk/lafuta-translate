58d6809202a40d45824ed3cdbc0aeb1276544910
potionk <48196324+potionk@users.noreply.github.com>
2020-05-16 18:51:24 +0900

    Update translation function using papago api

--- src/views/Translate/Translate.js
+++ src/views/Translate/Translate.js
@@ -1,62 +1,83 @@
 import React, {Component} from 'react';
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import Contribute from '../Contribute/Contribute';
-import {
-  Button,
-  Col,
-  Input,
-  Row,
-  Card,
-  CardBody,
-} from 'reactstrap';
+import axios from "axios";
+import {Button, Col, Input, Row, Card, CardBody} from 'reactstrap';
+const qs = require('querystring')
+
 class Translate extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      KoreanText:"",
-      Translated_Text: "" ,
+      korean_text:"",
+      translated_text: "" ,
       able_submit: false
     };
-    
     this.handleClick = this.handleClick.bind(this);
   };
   handleChange=(e)=>{
     this.setState({
-      KoreanText: e.target.value
+      korean_text: e.target.value
     })
+    console.log(this.state.korean_text)
   }
   handleClick=(e)=>{
-    let dummy_text = "Deep learning has emerged as a new area of machine-learning research and has been successfully applied to natural language processing, such as machine translation and sentence classification. In this work, we use effective Korean input token units to encode Korean sentences for classification problems, such as topic detection. Recurrent and convolutional neural networks for Korean sentence encoding are briefly reviewed, and various Korean input tokens units, including character, morpheme-tag, morpheme, word, subword, syllable window, and hybrids of morpheme and character methods are explored. Extensive experiments on sentimental analysis, topic detection, and intention understanding tasks are conducted to find effective input token units.";
     this.setState({
-      Translated_Text:dummy_text,
+      translated_text: this.state.korean_text,
       able_submit:true
     });
+    this.translate();
   }
   handleSubmit=(e)=>{                                                                                     
     this.setState({
-      Translated_Text:this.state.KoreanText,
-      Korean : ""
+      translated_text:this.state.korean_text,
     });
   }
+
+  translate = async () => {
+    const headers = {
+      'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Accept': '*/*',
+      'X-Naver-Client-Id': '48do5KkeVQYZBNxkJGUt',
+      'X-Naver-Client-Secret': 'vIXAEug1_8'
+    }
+    let param = {
+      source: 'ko',
+      target: 'en',
+      text: this.state.korean_text
+    }
+
+    axios.post("/v1/papago/n2mt", qs.stringify(param), {headers: headers})
+      .then(res => {
+        let english_text = res.data;
+        console.log(english_text.message.result.translatedText);
+        this.setState({
+          translated_text: english_text.message.result.translatedText,
+        });
+      }).catch(error => {
+        console.log('failed', error)
+      })
+  };
+
   render() {
     return (
     <div>
       <Row>
         <Col>
-          <Input type="textarea" value={this.state.KoreanText} 
+          <Input type="textarea" value={this.state.korean_text} 
           onChange={this.handleChange} rows="9"/>
           <Button onClick={this.handleClick}>Translate</Button>
         </Col>
         <Col>
           <Card>
             <CardBody>
-              {this.state.Translated_Text}
+              {this.state.translated_text}
             </CardBody>
           </Card>
           <Link to={{pathname: '/contribute',
             state: {
-              KoreanText: this.state.KoreanText,
-              Translated_Text: this.state.Translated_Text
+              korean_text: this.state.korean_text,
+              translated_text: this.state.translated_text
           }}}>
             <Button disabled={!this.state.able_submit} onClick={this.handleSubmit}>Contribute</Button>
           </Link>
